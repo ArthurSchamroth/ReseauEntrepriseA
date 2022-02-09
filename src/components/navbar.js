f7af4c155f5c1a1e9b558f77945712509203e2f9
vbrichant <56791491+vbrichant@users.noreply.github.com>
2022-02-09 13:49:11 +0100

    Suppression de l'inscription de la navbar + ajout de la redirection depuis la page login vers la page inscription + création de la page Formateur.js

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -16,6 +16,7 @@ const NavBar = () => {
                 </Navbar.Brand>
                 <Nav className="me-auto">
                     <Link to="/Formation">Formations</Link>
+                    <Link to="/Fromateur">Formateur</Link>
                     <Link to="/Login">Login</Link>
                 </Nav>
             </Container>

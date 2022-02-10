2f2dd20f1204bd35dafe545725525b8c3c6a2d57
vbrichant <56791491+vbrichant@users.noreply.github.com>
2022-02-10 10:41:12 +0100

    traitement des requete d'inscription et de login dans le backend

---
+++ backend/controllers/user.controller.js
@@ -0,0 +1,33 @@
+const user = require('../model/user.model');
+const mysql = require('../model/db');
+
+exports.inscription = (req, res) => {
+    user.inscription((err, data) => {
+        console.log(req)
+        if (err) {
+
+            res.status(500).send({
+                message: "Error during the register ",
+            });
+
+        } else {
+            res.header("Access-Control-Allow-Origin", "*");
+            res.send(data);
+        }
+    });
+};
+exports.login = (req, res) => {
+    user.login((err, data) => {
+        console.log(req)
+        if (err) {
+
+            res.status(500).send({
+                message: "Wrong password or username",
+            });
+
+        } else {
+            res.header("Access-Control-Allow-Origin", "*");
+            res.send(data);
+        }
+    });
+};
